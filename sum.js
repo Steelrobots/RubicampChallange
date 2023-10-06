8b87b98c25dd4667dcc2f7dee9fdae112953fdc0
Andri Priyadi <dripriyadi@gmail.com>
2023-10-06 14:23:53 +0700

    fix

--- sum.js
+++ sum.js
@@ -1,6 +1,12 @@
 function sum(){
+    let result = 0;
+    for (let i = 0; i < arguments.length; i++) {    
+        result = result + arguments[i]  
+        
+        
+    }
 
-
+    console.log(result);
 }
 
 sum(1,2,7);
