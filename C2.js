85713cc1358963c382beefde91df27ca7ce53ca6
Andri Priyadi <dripriyadi@gmail.com>
2023-10-07 07:23:42 +0700

    first push

---
+++ C2.js
@@ -0,0 +1,19 @@
+function deretKaskus(n){
+
+    let kaliTiga = []
+    for (let i = 1; i <= n; i++) {
+        if((i * 3) % 5 === 0){
+            kaliTiga.push('KAS')
+        } else if((i * 3) % 6 === 0){
+            kaliTiga.push('KUS')
+        } else if((i * 3) % 5 === 0 && (i * 3) % 6 === 0){
+            kaliTiga.push('KASKUS')
+        } else {
+            kaliTiga.push((i * 3))
+        }
+        
+    } return kaliTiga;
+
+}
+
+console.log(deretKaskus(10));
