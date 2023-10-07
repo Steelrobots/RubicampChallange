8ebedd74fbde417bff21e8631036b96465eaab39
Andri Priyadi <dripriyadi@gmail.com>
2023-10-07 10:50:06 +0700

    coba

--- C3.js
+++ C3.js
@@ -1,19 +1,8 @@
 function romawi(n){
     
-const tabel = [
-    { awal: 1000, akhir: 'M' },
-    { awal: 900, akhir: 'CM' },
-    { awal: 500, akhir: 'D' },
-    { awal: 400, akhir: 'CD' },
-    { awal: 100, akhir: 'C' },
-    { awal: 90, akhir: 'XC' },
-    { awal: 50, akhir: 'L' },
-    { awal: 40, akhir: 'XL' },
-    { awal: 10, akhir: 'X' },
-    { awal: 9, akhir: 'IX' },
-    { awal: 5, akhir: 'V' },
-    { awal: 4, akhir: 'IV' },
-    { awal: 1, akhir: 'I' }
+const tabel = [{ awal: 1000, akhir: 'M' },{ awal: 900, akhir: 'CM' },{ awal: 500, akhir: 'D' },{ awal: 400, akhir: 'CD' },{ awal: 100, akhir: 'C' },
+    { awal: 90, akhir: 'XC' },{ awal: 50, akhir: 'L' },{ awal: 40, akhir: 'XL' },{ awal: 10, akhir: 'X' },{ awal: 9, akhir: 'IX' },
+    { awal: 5, akhir: 'V' }, { awal: 4, akhir: 'IV' },{ awal: 1, akhir: 'I' }
 ];     
   
     let jadi = '';
