ef9db6da5a77098ed714c10e8ea7734c600283ea
Andri Priyadi <dripriyadi@gmail.com>
2023-10-09 21:49:45 +0700

    fixed

---
+++ C6-sentenceManipulation.js
@@ -0,0 +1,15 @@
+function sentenceManipulation(sentence){
+    let arrSent = sentence.split(" ");
+    let arrNew = [];
+    
+    for (let i = 0; i < arrSent.length; i++) {
+        if(arrSent[i].charAt(0).match(/[aiueoAIUEO]/)){
+            arrNew.push(arrSent[i]);
+        } else {
+            arrNew.push(arrSent[i].slice(1).concat(arrSent[i].charAt(0)).concat('nyo'))
+        }
+        
+        
+    }console.log(arrNew.join(' '));
+}
+sentenceManipulation('Ibu pergi ke pasar bersama aku');
