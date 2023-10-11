d31ad81d8325dfae5846b0d3d0771747cd2bdfa0
Andri Priyadi <dripriyadi@gmail.com>
2023-10-11 10:10:03 +0700

    edited

--- C5.js
+++ C5.js
@@ -1,8 +1,8 @@
 function stringManipulation(word){
-    if(word.charAt(0).match(/[aiueoAIUEO]/)){
+    if(word[0].match(/[aiueoAIUEO]/)){
         console.log(word)
     } else {
-        let newWord = word.slice(1,word.length).concat(word.charAt(0)).concat('nyo');
+        let newWord = word.slice(1,word.length).concat(word[0]).concat('nyo');
         console.log(newWord);
     }
 }
