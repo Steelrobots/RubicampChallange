4ef68b77e91194e4045ce5bc35909a92389e8b49
Andri Priyadi <dripriyadi@gmail.com>
2023-10-15 07:52:58 +0700

    test

--- C9-Spiral.js
+++ C9-Spiral.js
@@ -1,43 +1,44 @@
 function spiral(param1) {
-    var matrix = [];
-    let count = 0
-  for(let i = 0; i < param1; i++) {
-     matrix[i] = [];
-  for(let j=0; j < param1;j++) {
-       matrix[i][j] = count;
-       count++
-    }
-   }    //console.log(matrix)  membuat array untuk di spiral   
-
-   let result = [];
-   let start = 0;
-   let end = param1 - 1
-   let number = 1;
-
-   while (result.length < param1 * param1){
-
-    for (let x = start; x < matrix.length; x++) {
-      result.push(matrix[start][x]);      
-    }
-    for (let y = number; y < matrix.length; y++) {
-      result.push(matrix[y][end]);
-      
-    }
-    for (let z = 0; z < matrix.length; z++) {
-      result.push(matrix[end][z]);
-    }
-    for (let w = 0; w < matrix.length; w++) {
-      result.push(matrix[w][start]);      
-    }
-    start++;
-    number++;
-    end--;
-    matrix.length--;
-   }
-   console.log(result);
+  let array = [];
+  let hasil = [];
+  let baru = 0;
 
 
-}                  
+  for (let i = 0; i < param1; i++) {
+      array[i] = [];
+      for (let j = 0; j < param1; j++) {
+          array[i][j] = baru;
+          baru++;
+      }
+  }
+  let frontRow = 0;
+  let frontCol = 0;
+  let lastCol = param1 - 1;
+  let lastRow = param1 - 1;
+  while (frontRow <= lastRow && frontCol <= lastCol) {
+      for (let j = frontRow; j <= lastCol; j++) {
+          hasil.push(array[frontRow][j])
+      }
+      frontRow++
+      for (let k = frontRow; k <= lastRow; k++) {
+          hasil.push(array[k][lastCol]);
+      }
+      lastCol--
+
+      for (let l = lastCol; l >= frontCol; l--) {
+          hasil.push(array[lastRow][l]);
+      }
+      lastRow--
+
+      for (let m = lastRow; m >= frontRow; m--) {
+          hasil.push(array[m][frontRow]);
+      }
+      frontRow++
+  }
+
+  console.log(hasil);
+}
+
 spiral(5);
 spiral(6);
 spiral(7);
