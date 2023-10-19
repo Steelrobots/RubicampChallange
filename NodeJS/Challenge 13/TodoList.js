85f38c09f6ed54211bb049c69fd6bdbad3f235c0
Andri Priyadi <dripriyadi@gmail.com>
2023-10-19 19:22:33 +0700

    finished

--- NodeJS/Challenge 13/TodoList.js
+++ NodeJS/Challenge 13/TodoList.js
@@ -20,77 +20,118 @@ $ node TodoList.js filter:<tag_name>
 `
 
 // console.log(daftarIsi);
-if (!command) {
-    console.log(daftarIsi)
-} else {
-    switch (command) {
-        case 'help':
-            console.log(daftarIsi)
-            break;
-        case 'list':
+switch (command) {
+    case 'help':
+        console.log(daftarIsi)
+        break;
+
+    case 'list':
+        console.log("Daftar Pekerjaan")
+        for (let x of obj) {
+            if (x.complete) {
+                x.complete = "[x]"
+                console.log(`${x.ID}. ${x.complete}. ${x.title}.`);
+            } else {
+                x.complete = "[ ]"
+                console.log(`${x.ID}. ${x.complete}. ${x.title}.`);
+            }
+        }
+        break;
+
+    case 'task':
+
+        for (let x in obj[hapus]) console.log(`${x}: ${obj[id - 1][x]}`);
+
+        break;
+
+    case 'add':
+
+        if (inform) {
+            console.log(`${inform} telah ditambahkan.`);
+            obj.push({ "ID": tambah, "title": inform, "complete": false, tag: "" });
+            fs.writeFileSync("./toDo.json", JSON.stringify(obj), "utf-8");
+        } else if (!inform || inform == " ") return;
+
+        break;
+
+    case 'delete':
+
+        console.log(`"${obj[hapus].title}" telah dihapus dari daftar`);
+        obj.splice(hapus, 1);
+        fs.writeFileSync("./toDo.json", JSON.stringify(obj), "utf-8");
+
+        break;
+
+    case 'complete':
+
+        console.log(`"${obj[hapus].title}" telah selesai.`);
+        obj[id - 1].complete = true;
+        fs.writeFileSync("./toDo.json", JSON.stringify(obj), "utf-8");
+
+        break;
+
+    case 'uncomplete':
+        console.log(`"${obj[hapus].title}" status selesai dibatalkan.`);
+        obj[id - 1].complete = false;
+        fs.writeFileSync("./toDo.json", JSON.stringify(obj), "utf-8");
+        break;
+
+    case 'list:outstanding':
+        console.log('Daftar Pekerjaan');
+        let outstanding = [];
+        for (let x of obj) {
+            if (!x.complete) {
+                x.complete = "[]";
+                outstanding.push(`${x.ID}. ${x.complete}. ${x.title}.`);
+            }
+        }
+        if (id === 'asc') {
+            console.log(outstanding.join('\n'))
+        } else if (id === "desc") {
+            console.log(outstanding.reverse().join('\n'));
+        }
+        break;
+
+    case 'list:completed':
+        console.log('Daftar Pekerjaan');
+        let completed = []
+        for (let x of obj) {
+            if (x.complete) {
+                x.complete = "[x]";
+                completed.push(`${x.ID}. ${x.complete}. ${x.title}.`);
+            }
+        }
+        if (id === 'asc') {
+            console.log(completed.join('\n'))
+        } else if (id === "desc") {
+            console.log(completed.reverse().join('\n'));
+        }
+        break;
+    case 'tag':
+        console.log(`"Tag ${process.argv.slice(4)}" telah ditambahkan ke dalam '${obj[obj.findIndex(x => x.ID == id)].title} `);
+        obj[hapus].tag = process.argv.slice(4);
+
+        fs.writeFileSync("./toDo.json", JSON.stringify(obj), "utf-8");
+        break;
+
+
+    default:
+        if (!command || command == 'help') {
+            console.log(daftarIsi);
+        }
+        else if (command.toLowerCase().startsWith(`filter:${command.slice(7)}`)) {
             console.log("Daftar Pekerjaan")
             for (let x of obj) {
-                if (x.complete) {
-                    x.complete = "[x]"
-                    console.log(`${x.ID}. ${x.complete}. ${x.title}.`);
-                } else {
-                    x.complete = "[ ]"
-                    console.log(`${x.ID}. ${x.complete}. ${x.title}.`);
+                if (x.tag.includes(command.slice(7))) {
+                    if (x.complete) {
+                        x.complete = "[x]";
+                        console.log(`${x.ID}. ${x.complete}. ${x.title}`)
+                    } else if (!x.complete) {
+                        x.complete = "[ ]"
+                        console.log(`${x.ID}. ${x.complete}. ${x.title}`)
+                    }
                 }
             }
-            break;
-        case 'task':
-
-            for (let x in obj[hapus]) console.log(`${x}: ${obj[id - 1][x]}`);
-
-            break;
-        case 'add':
-
-            if (inform) {
-                console.log(`${inform} telah ditambahkan.`);
-                obj.push({ "ID": tambah, "title": inform, "complete": false, tag: "" });
-                fs.writeFileSync("./toDo.json", JSON.stringify(obj), "utf-8");
-            } else if (!inform || inform == " ") return;
-
-            break;
-        case 'delete':
-
-            console.log(`"${obj[id - 1].title}" telah dihapus dari daftar`);
-            obj.splice(hapus, 1);
-            fs.writeFileSync("./toDo.json", JSON.stringify(obj), "utf-8");
-
-            break;
-        case 'complete':
-
-            console.log(`"${obj[id - 1].title}" telah selesai.`);
-            obj[id - 1].complete = true;
-            fs.writeFileSync("./toDo.json", JSON.stringify(obj), "utf-8");
-
-            break;
-        case 'uncomplete':
-            console.log(`"${obj[id - 1].title}" status selesai dibatalkan.`);
-            obj[id - 1].complete = false;
-            fs.writeFileSync("./toDo.json", JSON.stringify(obj), "utf-8");
-            break;
-        case 'list:outstanding' :
-            console.log('Daftar Pekerjaan');
-            let outstanding = [];
-            for(let x of obj){
-                if(!x.complete){
-                    x.complete = "[]";
-                    outstanding.push(`${x.ID}. ${x.complete}. ${x.title}.`);
-                }
-            }
-            if(id === 'asc'){
-                console.log(outstanding.join('\n'))
-            }else if (id === "desc"){
-                console.log(outstanding.reverse().join('\n'));
-            }
-            break;
-
-
-        default:
-            break;
-    }
+        }
+        break;
 }
-
